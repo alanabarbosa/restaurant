418c4b2bd070ac10125069a2f764df4c1d944f7c
alanabarbosa <itsalanayall@hotmail.com>
2021-05-19 19:24:45 -0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100
+});
+wow.init();
+document.getElementById('').onclick = function() {
+    var section = document.createElement('section');
+    section.className = 'wow fadeInDown';
+    section.className = 'wow shake';
+    section.className = 'wow zoomIn';
+    section.className = 'wow lightSpeedIn';
+    this.parentNode.insertBefore(section, this);
+};
